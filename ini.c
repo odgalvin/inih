a2c159e0a8d0af83fc3c394e41f3c4108646b2e9
Oliver Galvin <odg@riseup.net>
2018-07-23 15:36:46 +0100

    Don't need to return from strncpy0

--- ini.c
+++ ini.c
@@ -68,11 +68,10 @@ static char* find_chars_or_comment(const char* s, /*@null@*/ const char* chars)
 }
 
 /* Version of strncpy that ensures dest (size bytes) is null-terminated. */
-static char* strncpy0(char* dest, const char* src, size_t size)
+static void strncpy0(char* dest, const char* src, size_t size)
 {
     strncpy(dest, src, size - 1);
     dest[size - 1] = '\0';
-    return dest;
 }
 
 /* See documentation in header file. */
